6efec49c0ea13b15906ab10a609ca1c5f4a585d2
jan310 <jan310.ondra@gmail.com>
2023-12-17 12:17:02 +0100

    (BE) created project

---
+++ knowledge-base-be/src/main/java/com/ondra/knowledgebasebe/KnowledgeBaseBeApplication.java
@@ -0,0 +1,13 @@
+package com.ondra.knowledgebasebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeBaseBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeBaseBeApplication.class, args);
+    }
+
+}
---
+++ knowledge-base-be/src/test/java/com/ondra/knowledgebasebe/KnowledgeBaseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ondra.knowledgebasebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeBaseBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
